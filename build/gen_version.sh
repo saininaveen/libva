52f5eeb96b221bcc0e7c53b7cf72baa5747bd8e6
Sebastian Ramacher <sebastian@ramacher.at>
2017-04-01 18:09:33 +0200

    New upstream version 1.8.0

--- build/gen_version.sh
+++ build/gen_version.sh
@@ -20,14 +20,35 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-ACLOCAL_AMFLAGS = -I m4 ${ACLOCAL_FLAGS}
+#!/bin/sh
 
-AUTOMAKE_OPTIONS = foreign
+libva_topdir="$1"
+version_h_in="$2"
 
-SUBDIRS = va dummy_drv_video pkgconfig doc
+parse_configure_ac() {
+    sed -n "/^m4_define.*${1}.*\[\([0-9]*\)\].*/s//\1/p" ${libva_topdir}/configure.ac
+}
 
-# Extra clean files so that maintainer-clean removes *everything*
-MAINTAINERCLEANFILES = \
-	aclocal.m4 compile config.guess config.sub \
-	configure depcomp install-sh ltmain.sh     \
-	Makefile.in missing
+parse_configure() {
+    sed -n "/^${1}=\([0-9]*\)/s//\1/p" ${libva_topdir}/configure
+}
+
+if test -f "${libva_topdir}/configure.ac"; then
+    va_api_major_version=`parse_configure_ac va_api_major_version`
+    va_api_minor_version=`parse_configure_ac va_api_minor_version`
+    va_api_micro_version=`parse_configure_ac va_api_micro_version`
+elif test -f "${libva_topdir}/configure"; then
+    va_api_major_version=`parse_configure VA_API_MAJOR_VERSION`
+    va_api_minor_version=`parse_configure VA_API_MINOR_VERSION`
+    va_api_micro_version=`parse_configure VA_API_MICRO_VERSION`
+else
+    echo "ERROR: configure or configure.ac file not found in $libva_topdir/"
+    exit 1
+fi
+va_api_version="$va_api_major_version.$va_api_minor_version.$va_api_micro_version"
+
+sed -e "s/@VA_API_MAJOR_VERSION@/${va_api_major_version}/" \
+    -e "s/@VA_API_MINOR_VERSION@/${va_api_minor_version}/" \
+    -e "s/@VA_API_MICRO_VERSION@/${va_api_micro_version}/" \
+    -e "s/@VA_API_VERSION@/${va_api_version}/" \
+    $version_h_in
